1a9f8be4c007785b4714d45e7c0c18772dc0ecbb
smudesign <urwinsamantha@gmail.com>
2021-09-24 15:32:17 -0400

    textarea colour changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,36 @@ var displayDate = function() {
 };
 
 // colourCoded
+// setInterval to check if an event is past, present or future every hour
+setInterval (function(){
+    colourCoded();
+}, (1000 * 60) * 60);
+
+// function to change colour of time blocks
+var colourCoded = function() {
+    // for each element with the id of .description
+    $('.description').each(function() {
+        // create variable for the time of the event
+        var eventTime = $(this).attr("id");
+
+        // create variable for current time
+        var currentTime = moment().hour();
+
+        // create conditional statement to change colour of textarea
+        if (currentTime === eventTime) {
+            $(this).removeClass();
+            $(this).addClass("present col-10 description");
+        } else if (currentTime > eventTime) {
+            $(this).removeClass();
+            $(this).addClass("past col-10 description");
+        } else if (currentTime < eventTime) {
+            $(this).removeClass();
+            $(this).addClass("future col-10 description");
+        }
+    });
+};
 
 // saveEvents
 
 displayDate();
+colourCoded();
